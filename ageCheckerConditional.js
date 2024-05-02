fbe045916eb8064f30e2e8e51de5c254288e61c6
Jamie Brogdon <125220298+jamiebrogdon@users.noreply.github.com>
2024-05-01 22:22:42 -0500

    Create ageCheckerConditional.js

---
+++ ageCheckerConditional.js
@@ -0,0 +1,25 @@
+//Create a conditonal that checks their age
+//If under 16, tell them they can not drive
+//If under 18, tell them they can't hate from outside the club, because they can't even get in
+//If under 21, tell them they can not drink
+//If under 25, tell them they can not rent cars affordably
+//If under 30, tell them they can not rent fancy cars affordably
+//If under over 30, tell them there is nothing left to look forward too
+
+let driversAge = ''
+
+if(driversAge < 16) {
+    console.log("you can not drive")
+}else if(driversAge < 18){
+    console.log("cant hate from outside the club, because they cant even get in")
+}else if(driversAge < 21){
+    console.log("ya not drink")
+}else if(driversAge < 25){
+    console.log("you can not rent cars affordably")
+}else if(driversAge < 30){
+    console.log("You can not rent fancy cars affordably")
+}else if(driversAge = 30 || driversAge > 30 ){
+    console.log("Nothing left to look forward to BUT 100 devs baby!")
+}else{
+    console.log("Its okaaayyyyy yall")
+};
